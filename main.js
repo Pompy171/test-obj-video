16bf8efaf55190952aa6cb95778c4f236cfcfc9f
Pompy171 <77317518+Pompy171@users.noreply.github.com>
2021-11-03 17:35:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var video="";
+
+function preload(){
+    video=createVideo("video.mp4");
+    video.hide();
+
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelloaded);
+}
+
+
+function modelloaded(){
+    console.log("Model Loaded");
+}
